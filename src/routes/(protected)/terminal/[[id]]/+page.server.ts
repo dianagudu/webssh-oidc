8f692a5952cf8f9fc93e07384dc5fbe44f81c7d6
Diana Gudu <diana.gudu@kit.edu>
2024-06-09 21:21:40 +0200

    fix websocket path

--- src/routes/(protected)/terminal/[[id]]/+page.server.ts
+++ src/routes/(protected)/terminal/[[id]]/+page.server.ts
@@ -2,7 +2,7 @@ import type { PageServerLoad } from './$types';
 
 export const load = (async ({ locals, params, url }) => {
 	const currentTerminalSession = locals.userSession.terminals.find((s) => s.id === params.id);
-	const wsUrl = `${url.protocol === 'https:' ? 'wss' : 'ws'}://${url.hostname}:8445/ws`;
+	const wsUrl = `${url.protocol === 'https:' ? 'wss' : 'ws'}://${url.host}/ws`;
 
 	return {
 		terminalId: params.id,
